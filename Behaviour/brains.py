148f9f9bce4af8c8f93ed696702921ecc31f327a
Luis Perez <luisperez@college.harvard.edu>
2014-04-25 15:39:41 -0400

    Creating new files

---
+++ Behaviour/brains.py
@@ -0,0 +1,12 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Implementation of different robot brains. 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class ExampleBrain(object):
+  def __init__(self):
+    pass
+
+  # functions needed to be implemented for decision making go here
+
+class SimpleBrain(object):
+  def __init__(self):
+    pass
