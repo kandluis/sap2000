17fc256f1827e0488fd4532727017a343675c5fa
Max Kanwal <mkanwal@Maxs-MacBook-Pro.local>
2014-06-27 13:08:55 -0400

    angle modifications

---
+++ Behaviour/brain_v1.py
@@ -0,0 +1,86 @@
+# Python default libraries
+import random
+import math
+import pdb
+from abc import ABCMeta, abstractmethod
+
+# Local imports
+from Helpers import helpers
+# constants for simulation
+from variables import BEAM, ROBOT,PROGRAM, VISUALIZATION
+# constants for construction
+from construction import HOME,CONSTRUCTION
+# constants for behaviour
+from Behaviour import constants as BConstants
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Implementation of robot brain. 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+class BaseBrain:
+  __metaclass__=ABCMeta
+  @abstractmethod
+  def __init__(self,Robot):
+    # access to the robot body; functions are detailed in the documentation file
+    # Code for the robot can be found in /World/robot.py
+    self.Body = Robot
+
+  @abstractmethod
+  def performDecision(self):
+    '''
+    This function is called during the simulation before the act function is called.
+    The brain should perform the task of deciding what actions to perform next.
+
+    In particular, note that self.Body.addToMemory(key,value) allows for storing 
+    information in the Body. 
+    '''
+    pass
+
+  @abstractmethod
+  def performAction(self):
+    '''
+    Perform the action that should occur after each timestep. This actions should be 
+    based on the information currently stored in memory, as the robot has to access
+    to data from the structure. DO NOT ATTEMPT to access the SAP 2000 API; there
+    are no results.
+    '''
+    pass
+
+class Brain(BaseBrain):
+  def __init__(self,Robot):
+    super().__init__(Robot)
+
+  def performDecision(self):
+    #pdb.set_trace()
+    self.decide()
+
+  def performAction(self):
+    #pdb.set_trace()
+    self.do_action()
+
+  ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  Brain Helper functions
+  '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+  def decide(self):
+    pass
+
+  def do_action(self):
+    pass
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
