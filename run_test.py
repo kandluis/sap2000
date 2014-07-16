6d8e4e4f73328a74a43874a5f73bfc5d228ca974
Max Kanwal <maxowaxo2013@gmail.com>
2014-07-16 17:28:23 -0400

    fixed errors

--- run_test.py
+++ run_test.py
@@ -11,9 +11,9 @@ view = True
 # To turn on/off the use of SAP, go to Helpers/helpers.py and change the
 # global variable "SAP_PHYSICS" (defined after the import statements) to True/False.
 seed = "r@nd0M2"
-robot_number = 1
+robot_number = 4
 maxsteps = 10000
-debug = 0#264 # begin debugging AFTER this timestep. 0 turns off debugging, change to 1 to debug w/ PDB
+debug = 0 # begin debugging AFTER this timestep. 0 turns off debugging, change to 1 to debug w/ PDB
 comment = seed
 
 # seeding the simulation
