1abf0186421b5943f9336877e77eb8d84f67f1ea
Max Kanwal <maxowaxo2013@gmail.com>
2014-07-29 14:05:38 -0400

    fixed errors

--- vis_test.py
+++ vis_test.py
@@ -1,6 +1,6 @@
 from visualization import Visualization
 
-revisualize = 'C:\\SAP 2000\\2014-Jul\\Jul-27\\17_08_37\\'
+revisualize = 'C:\\SAP 2000\\2014-Jul\\Jul-29\\01_09_23\\'
 #'C:\\SAP 2000\\2014-Jul\\Jul-03\\ \\'
 # 16_42_37
 # 17_01_07
@@ -17,7 +17,8 @@ revisualize = 'C:\\SAP 2000\\2014-Jul\\Jul-27\\17_08_37\\'
 # 01_36_49
 # 10_26_30
 # 19_40_22
-
+#'C:\\SAP 2000\\2014-Jul\\Jul-29\\ \\'
+# 01_09_23
 
 vis = Visualization(revisualize)
 vis.load_data()
