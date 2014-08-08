6ca003656131ac3361023237745f52ecce782c66
Max Kanwal <maxowaxo2013@gmail.com>
2014-08-08 09:25:33 -0400

    new strategy test

--- World/robot.py
+++ World/robot.py
@@ -705,13 +705,14 @@ class Body(BaseBody):
         # So e1 is in the form (x,y,z)
         e1, e2 = box[name].endpoints 
         # beam is lying on the ground (THIS IS NOT FUNCTIONAL)
+        '''
         if helpers.compare(e1[2],0) and helpers.compare(e2[0],0):
           # pdb.set_trace()
           vectors[name] = helpers.vector_to_line(e1,e2,self.location)
           distances[name] = helpers.length(vectors[name])
-
+        '''
         # Only one point is on the ground
-        elif helpers.compare(e1[2],0):
+        if helpers.compare(e1[2],0):
           vectors[name] = helpers.make_vector(self.location, e1)
           distances[name] = helpers.distance(e1, self.location)
         elif helpers.compare(e2[2],0):
